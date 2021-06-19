1a3ebcc6b2cfe1e24098f4cf730b61a06acfc1b1
Vaishnavi-dhanya <vaishnavidhanya1999@gmail.com>
2021-06-19 12:13:16 +0530

    max

--- solns.c
+++ solns.c
@@ -1,24 +1,7 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
-int max(int x[], int  num_x){
-   
-   int max_x;
-   
-   if(num_x==1){
-      return x[0];
-   }
-   else{
-   max_x=x[0];
-    
-   for(int i=1;i<num_x;i++){
-      if(x[i]>max_x){
-          max_x=x[i];
-      }
-     }
-   return max_x;
-   }
-   }
+
 int min(int x[], int num_x){
 
     int min_x;
@@ -43,6 +26,24 @@ float average(int x[], int num_x){
    }
    return sum/num_x;
 }
+
+int max(int x[], int  num_x){
+   
+   int max_x;
+   max_x=x[0];
+    
+   for(int i=0;i<num_x;i++){
+      if(x[i]>max_x){
+          max_x=x[i];
+      }
+     }
+   return max_x;
+   
+   
+}
+     
+    
+
 
 
 
