f72b069723ec83f6caf1e85907a55dd220a7f19f
Vaishnavi-dhanya <vaishnavidhanya1999@gmail.com>
2021-06-19 11:58:54 +0530

    maximum

--- solns.c
+++ solns.c
@@ -4,14 +4,20 @@
 int max(int x[], int  num_x){
    
    int max_x;
+   
+   if(num_x==1){
+      return x[0];
+   }
+   else{
    max_x=x[0];
     
-   for(int i=0;i<num_x;i++){
+   for(int i=1;i<num_x;i++){
       if(x[i]>max_x){
           max_x=x[i];
       }
      }
    return max_x;
+   }
    }
 int min(int x[], int num_x){
 
