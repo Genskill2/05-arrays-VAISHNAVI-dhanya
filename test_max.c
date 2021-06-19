6fd69b23fc59febaa5b7306fb3f21f79182b50d3
Vaishnavi-dhanya <vaishnavidhanya1999@gmail.com>
2021-06-19 11:38:27 +0530

    maximum passed

--- test_max.c
+++ test_max.c
@@ -4,6 +4,21 @@
 
 int max(int [], int);
 
+int max(int x[], int  num_x){
+   
+   int max_x;
+   max_x=x[0];
+    
+   for(int i=0;i<num_x;i++){
+      if(x[i]>max_x){
+          max_x=x[i];
+      }
+     }
+   return max_x;
+   
+   
+}
+
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (max(x, 7) == 10);
